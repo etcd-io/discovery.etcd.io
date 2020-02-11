1a03c1c1f939acc369e93ea77118c8a71f61c701
Rodolfo Rodriguez <me@rodolforodriguez.dev>
2020-02-11 06:38:50 -0600

    Public anonymous grafana (#39)
    * Hosts Grafana publicly w/anon users
    
    * Adds deploy script for prometheus-operator chart
    
    * Adds grafana link to README

---
+++ kubernetes/helm/prometheus/deploy.sh
@@ -0,0 +1,9 @@
+env=$1
+version="8.3.3"
+
+helm3 upgrade prometheus-operator stable/prometheus-operator \
+        --install --debug --atomic \
+        --timeout 2m \
+        --namespace prometheus \
+        --version $version \
+        --values values.yaml,$env.values.yaml
