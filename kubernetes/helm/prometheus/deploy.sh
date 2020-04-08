2f08f594045bc0b728876ee05ccd690b5cc89080
Rodolfo Rodriguez <rodolfo@cloudkite.io>
2020-04-08 06:39:45 -0500

    Updates alertmanager config, deploys alertmanager.tmpl to secret

--- kubernetes/helm/prometheus/deploy.sh
+++ kubernetes/helm/prometheus/deploy.sh
@@ -1,7 +1,7 @@
 env=$1
 version="8.3.3"
 
-helm3 upgrade prometheus-operator stable/prometheus-operator \
+helm3 upgrade prometheus-operator . \
         --install --debug --atomic \
         --timeout 2m \
         --namespace prometheus \
