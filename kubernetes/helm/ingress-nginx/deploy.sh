824232780f63b88db0fd38bfea3a6f21e9775a47
David Essien <davidessienshare@gmail.com>
2021-08-17 17:59:03 +0100

    fix kube-prometheus-stack

--- kubernetes/helm/ingress-nginx/deploy.sh
+++ kubernetes/helm/ingress-nginx/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 3.34.0 --atomic --debug
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 3.35.0 --atomic --debug
