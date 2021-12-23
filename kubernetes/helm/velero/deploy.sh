ae0b3a11f11684eb6e476f970a52375a28396275
David Essien <davidessienshare@gmail.com>
2021-12-23 16:18:53 +0100

    upgrade velero

--- kubernetes/helm/velero/deploy.sh
+++ kubernetes/helm/velero/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 env=$1
-version=2.23.1
+version=2.27.1
 
 helm upgrade velero vmware-tanzu/velero --install --create-namespace --namespace velero -f ${env}.values.yaml --version ${version} --atomic --debug
