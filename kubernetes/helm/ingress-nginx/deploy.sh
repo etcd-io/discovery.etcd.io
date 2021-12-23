2c1fa817fe197690fd1b013862790c27c00dd4d3
David Essien <davidessienshare@gmail.com>
2021-12-23 16:25:42 +0100

    upgrade nginx-ingress to ingress-nginx

--- kubernetes/helm/ingress-nginx/deploy.sh
+++ kubernetes/helm/ingress-nginx/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 3.35.0 --atomic --debug
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 4.0.13 --atomic --debug
