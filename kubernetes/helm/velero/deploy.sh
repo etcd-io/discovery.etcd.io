98533a20b9c4d034dabd535ebb7bbf204db7ebde
David Essien <davidessienshare@gmail.com>
2022-05-11 13:31:17 +0100

    update discovery helm charts
    - update cert-manager to v1.8.0
    - update velero to v1.8.1
    - update kube-prometheus-stack to v0.56.2

--- kubernetes/helm/velero/deploy.sh
+++ kubernetes/helm/velero/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 env=$1
-version=2.27.1
+version=2.29.4
 
 helm upgrade velero vmware-tanzu/velero --install --create-namespace --namespace velero -f ${env}.values.yaml --version ${version} --atomic --debug
