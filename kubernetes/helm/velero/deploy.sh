dac4aa595cc4bf77245646a3c6b3001500bdb267
eduartua <eduartua@gmail.com>
2022-08-09 11:57:31 -0500

    Upgrade velero to latest version

--- kubernetes/helm/velero/deploy.sh
+++ kubernetes/helm/velero/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 env=$1
-version=2.29.4
+version=2.30.2
 
 helm upgrade velero vmware-tanzu/velero --install --create-namespace --namespace velero -f ${env}.values.yaml --version ${version} --atomic --debug
