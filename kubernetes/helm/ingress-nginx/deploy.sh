21b2a821a10a80a70456455540853438d7c049d9
David Essien <davidessienshare@gmail.com>
2023-03-02 11:33:10 +0100

    upgrade ingress nginx to v1.6.4
    Signed-off-by: David Essien <davidessienshare@gmail.com>

--- kubernetes/helm/ingress-nginx/deploy.sh
+++ kubernetes/helm/ingress-nginx/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 4.1.4 --atomic --debug
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 4.5.2 --atomic --debug
