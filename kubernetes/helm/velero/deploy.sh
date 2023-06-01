1c518ac68c8b9de0f4ec63982a600827d0d24e57
David Itam Essien <david@cloudkite.io>
2023-06-01 17:29:08 +0100

    Upgrade velero 1.11 (#72)
    Upgrade to velero 1.11
    
    ---------
    
    Signed-off-by: David Essien <david@cloudkite.io>
    Co-authored-by: mmd-afegbua <momodu@cloudkite.io>
    Signed-off-by: David Essien <david@cloudkite.io>

--- kubernetes/helm/velero/deploy.sh
+++ kubernetes/helm/velero/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 env=$1
-version=3.1.2
+version=4.0.2
 
 helm upgrade velero vmware-tanzu/velero --install --create-namespace --namespace velero -f ${env}.values.yaml --version ${version} --atomic --debug
