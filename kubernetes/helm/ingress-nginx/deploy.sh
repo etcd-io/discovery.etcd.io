1851d28a0d5ae33be59ea24302ad3581e1e4da07
David Essien <david@cloudkite.io>
2023-09-19 07:26:19 +0100

    update ingress nginx to 1.8.2

--- kubernetes/helm/ingress-nginx/deploy.sh
+++ kubernetes/helm/ingress-nginx/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 4.5.2 --atomic --debug
+helm upgrade ingress-nginx ingress-nginx/ingress-nginx --install --create-namespace --namespace ingress-nginx -f values.yaml --version 4.7.2 --atomic --debug
