b4254846db3c9047dac3a75e0939de1517defd1d
eduartua <eduartua@gmail.com>
2024-02-15 22:23:07 -0600

    Delete kube-prometheus-stack helm chart as Grafana is under AGPL-3.0
    Signed-off-by: eduartua <eduartua@gmail.com>

--- kubernetes/helm/kube-prometheus-stack/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-app=kube-prometheus-stack
-env=$1
-
-helm upgrade --atomic --debug --install --create-namespace --timeout 900s --namespace prometheus --values values.yaml,$env.values.yaml $app .
